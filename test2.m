48076f46249604b34da954bd2a0e0c7875cb1859
Suleman Sial <suleman.sial@student.uts.edu.au>
2023-10-14 16:08:27 +1100

    Experimenting with different poses for spawned objects

--- test2.m
+++ test2.m
@@ -3,28 +3,30 @@ clf;
 clc;
 axis equal
 hold on
-pose1{1} = transl(0.25,0,0);
-pose2{1} = transl(0.3,0,0);
-pose3{1} = transl(0.35,0,0);
-pose4{1} = transl(0.6,0,0);
-pose5{1} = transl(0.7,0,0);
-pose6{1} = transl(0.85,0,0)*trotz(3*pi/2);
-pose7{1} = transl(1.1,0,0)*trotz(3*pi/2);
-pose8{1} = transl(1.35,0,0)*trotz(3*pi/2);
-pose9{1} = transl(1.55,0,0);
-pose10{1} = transl(1.85,0,0);
-pose11{1} = transl(2.05,0,0);
-pose12{1} = transl(2.15,0,0);
+%% Initialise poses for manipulatable objects
+pencilPose{1} = transl(0.25,0,0);
+penPose{1} = transl(0.3,0,0);
+rulerPose{1} = transl(0.35,0,0);
+calcPose{1} = transl(0.6,0,0);
+EraserPose{1} = transl(0.7,0,0);
+bookChemPose{1} = transl(0.85,0,0)*trotz(3*pi/2);
+bookMathPose{1} = transl(1.1,0,0)*trotz(3*pi/2);
+bookEngPose{1} = transl(1.35,0,0)*trotz(3*pi/2);
+compassPose{1} = transl(1.55,0,0);
+keyboardPose{1} = transl(1.85,0,0);
+mousePose{1} = transl(2.05,0,0);
+ps5ControllerPose{1} = transl(2.15,0,0);
 
-RobotSpawn('Pencil',1,pose1)
-RobotSpawn('Pen',1,pose2)
-RobotSpawn('Ruler',1,pose3)
-RobotSpawn('Calculator',1,pose4)
-RobotSpawn('Eraser',1,pose5)
-RobotSpawn('BookChem',1,pose6)
-RobotSpawn('BookMath',1,pose7)
-RobotSpawn('BookEng',1,pose8)
-RobotSpawn('Compass',1,pose9)
-RobotSpawn('Keyboard',1,pose10)
-RobotSpawn('Mouse',1,pose11)
-RobotSpawn('PS5_Controller',1,pose12)
+%% Spawn in robot objects
+RobotSpawn('Pencil',1,pencilPose)
+RobotSpawn('Pen',1,penPose)
+RobotSpawn('Ruler',1,rulerPose)
+RobotSpawn('Calculator',1,calcPose)
+RobotSpawn('Eraser',1,EraserPose)
+RobotSpawn('BookChem',1,bookChemPose)
+RobotSpawn('BookMath',1,bookMathPose)
+RobotSpawn('BookEng',1,bookEngPose)
+RobotSpawn('Compass',1,compassPose)
+RobotSpawn('Keyboard',1,keyboardPose)
+RobotSpawn('Mouse',1,mousePose)
+RobotSpawn('PS5_Controller',1,ps5ControllerPose)
