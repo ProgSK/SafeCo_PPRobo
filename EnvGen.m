4790b679e406c1a4057a0c38296b8e30936a2d4c
SK_Prog <soorya.konath@student.uts.edu.au>
2023-10-27 17:21:36 +1100

    Additional loads added, shelf model enlarged

--- EnvGen.m
+++ EnvGen.m
@@ -39,7 +39,7 @@ end
 %% OBJECT POSE ALLOCATION
  
 % STEP 1: Assigning the Initial Pose Values for the various loads (position of origin before animated movement)
-    % pencilPose{1} = transl(-0.57,+0.2,+0.2);
+    pencilPose{1} = transl(0.23,2,1);
     penPose{1} = transl(0.2,2,1);
     % rulerPose{1} = transl(-0.70,+0.35,+0);
     % calcPose{1} = transl(-0.55,+0.35,+0);
@@ -48,9 +48,9 @@ end
     bookChemPose{1} = transl(-0.95,2.255,1.5);
     bookMathPose{1} = transl(-1.05,2.255,1.5);
     bookEngPose{1} = transl(-1.15,2.255,1.5);
-    % keyboardPose{1} = transl(0,0,0);
-    % mousePose{1} = transl(+0.5,+0.25,+0);
-    ps5ControllerPose{1} = transl(-1.15,2.4,1.67);
+    keyboardPose{1} = transl(-1.1,2.4,1.67);
+    mousePose{1} = transl(-1.4,2.4,1.67);
+    ps5ControllerPose{1} = transl(-1.4,2.4,1.35);
 
 % STEP 2: Assigning the Destination Pose Values for the various loads (position after movement is completed)
     % pencilFinalPose{1} = transl(-0.37,-0.35,+0.2);
@@ -71,7 +71,7 @@ end
 if exist(spawnFileName,'file') == 2
     load spawnData.mat % Load pre-existing objects
     % plots the objects (with no colour, PURELY FOR DEBUGGING PURPOSES)
-    % plot3d(pencilObj.robotModel{1},0,'workspace',workspace,'view',[30,30],'delay',0,'noarrow','nowrist','notiles');
+    plot3d(pencilObj.robotModel{1},0,'workspace',workspace,'view',[30,30],'delay',0,'noarrow','nowrist','notiles');
     plot3d(penObj.robotModel{1},0,'workspace',workspace,'view',[30,30],'delay',0,'noarrow','nowrist','notiles');
     % plot3d(rulerObj.robotModel{1},0,'workspace',workspace,'view',[30,30],'delay',0,'noarrow','nowrist','notiles');
     % plot3d(calcObj.robotModel{1},0,'workspace',workspace,'view',[30,30],'delay',0,'noarrow','nowrist','notiles');
@@ -80,11 +80,11 @@ if exist(spawnFileName,'file') == 2
     plot3d(mathBookObj.robotModel{1},0,'workspace',workspace,'view',[30,30],'delay',0,'noarrow','nowrist','notiles');
     plot3d(engBookObj.robotModel{1},0,'workspace',workspace,'view',[30,30],'delay',0,'noarrow','nowrist','notiles');
     % plot3d(compassObj.robotModel{1},0,'workspace',workspace,'view',[30,30],'delay',0,'noarrow','nowrist','notiles');
-    % plot3d(keyboardObj.robotModel{1},0,'workspace',workspace,'view',[30,30],'delay',0,'noarrow','nowrist','notiles');
-    % plot3d(mouseObj.robotModel{1},0,'workspace',workspace,'view',[30,30],'delay',0,'noarrow','nowrist','notiles');
+    plot3d(keyboardObj.robotModel{1},0,'workspace',workspace,'view',[30,30],'delay',0,'noarrow','nowrist','notiles');
+    plot3d(mouseObj.robotModel{1},0,'workspace',workspace,'view',[30,30],'delay',0,'noarrow','nowrist','notiles');
     plot3d(ps5ControllerObj.robotModel{1},0,'workspace',workspace,'view',[30,30],'delay',0,'noarrow','nowrist','notiles');
 else
-    % pencilObj = RobotSpawn('Pencil',1,pencilPose);
+    pencilObj = RobotSpawn('Pencil',1,pencilPose);
     penObj = RobotSpawn('Pen',1,penPose);
     % rulerObj = RobotSpawn('Ruler',1,rulerPose);
     % calcObj = RobotSpawn('Calculator',1,calcPose);
@@ -93,11 +93,11 @@ else
     mathBookObj = RobotSpawn('BookMath',1,bookMathPose);
     engBookObj = RobotSpawn('BookEng',1,bookEngPose);
     % compassObj = RobotSpawn('Compass',1,compassPose);
-    % keyboardObj = RobotSpawn('Keyboard',1,keyboardPose);
-    % mouseObj = RobotSpawn('Mouse',1,mousePose);
+    keyboardObj = RobotSpawn('Keyboard',1,keyboardPose);
+    mouseObj = RobotSpawn('Mouse',1,mousePose);
     ps5ControllerObj = RobotSpawn('PS5_Controller',1,ps5ControllerPose);
     % save spawnData.mat pencilObj penObj rulerObj calcObj eraserObj chemBookObj mathBookObj engBookObj compassObj keyboardObj mouseObj ps5ControllerObj
-    save spawnData.mat penObj engBookObj chemBookObj mathBookObj ps5ControllerObj
+    save spawnData.mat pencilObj penObj engBookObj chemBookObj mathBookObj ps5ControllerObj keyboardObj mouseObj
 end
 
     
@@ -127,7 +127,7 @@ function envGen()
         PlaceObject('3D Models/BottleDesign.ply',[-1,1.6,0.9]);
 
     % STEP 4: Create/Import Shelf Model/s
-        PlaceObject('3D Models/WShelfDesign.ply',[-1.05,2.4,1.5]);
+        PlaceObject('3D Models/WShelfDesign1.ply',[-1.22,2.4,1.5]);
         % PlaceObject('3D Models/FShelfDesign.ply',[0,0,0]); If wall shelf isn't good enough
 
     % STEP 5: Create/Import Human Model
